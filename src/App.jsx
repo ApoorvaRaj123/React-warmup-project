c3b99e0725d3c350828f8877b58943de9d7a7963
ApoorvaRaj123 <99551949+ApoorvaRaj123@users.noreply.github.com>
2024-02-29 22:10:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,17 @@ function App() {
 
   return (
     <>
-      
+      <div className = "">
+        <nav classNAme = "">
+          <h2>Add Product</h2>
+          <span></span>
+          <h3>first</h3>
+          <span></span>
+          <h3>Second</h3>
+          <span></span>
+          <h3>third</h3>
+        </nav>
+      </div>
     </>
   )
 }

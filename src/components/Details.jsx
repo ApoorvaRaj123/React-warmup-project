2f4f64ce227f82ffe4cf839c8b461c6311a94d04
ApoorvaRaj123 <99551949+ApoorvaRaj123@users.noreply.github.com>
2024-03-04 22:13:06 +0530

    Update Details.jsx

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -7,7 +7,12 @@ function Details() {
 
   return (
     <>
-      
+      <div className="w-[70%] h-full bg-red-100 m-auto p-[10%]">
+        <img src="" alt="" />
+        <div className="">
+          <h1>Title</h1>
+        </div>
+      </div>
     </>
   )
 }

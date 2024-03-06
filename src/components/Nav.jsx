7a886824145f836a01fb36f16de4f65ed8259912
ApoorvaRaj123 <99551949+ApoorvaRaj123@users.noreply.github.com>
2024-03-06 22:19:56 +0530

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,34 @@
-import { useState } from 'react'
-
-
-
+import { React } from "react";
 
 function Nav() {
-
   return (
     <>
-      
+      <nav className="w-[15%] h-full bg-zinc-100 flex flex-col items-center pt-5">
+        <a
+          className=" px-3 py-4 rounded border-2 text-blue-400 border-blue-200 "
+          href="/create"
+        >
+          Add new product
+        </a>
+        <hr className="my-3 w-[80%]" />
+        <h1 className="text-xl font-semibold mb-3 w-[80%]">Categories</h1>
+        <ul className="w-[80%]">
+          <li className="flex items-center gap-3 mb-3">
+            <span className="rounded-full block w-[17px] h-[17px] bg-blue-200"></span>{" "}
+            Cat 1
+          </li>
+          <li className="flex items-center gap-3 mb-3">
+            <span className="rounded-full block w-[17px] h-[17px] bg-red-200"></span>{" "}
+            Cat 2
+          </li>
+          <li className="flex items-center gap-3 mb-3">
+            <span className="rounded-full block w-[17px] h-[17px] bg-green-200"></span>{" "}
+            Cat 3
+          </li>
+        </ul>
+      </nav>
     </>
-  )
+  );
 }
 
 export default Nav;

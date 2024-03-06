e7ef9841568f8bbeff315319a0301dd3dc0b8c2f
ApoorvaRaj123 <99551949+ApoorvaRaj123@users.noreply.github.com>
2024-03-06 22:19:09 +0530

    Update Details.jsx

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,20 +1,24 @@
-import { useState } from 'react'
-
-
-
+import { React } from "react";
+import { Link } from "react-router-dom";
 
 function Details() {
-
   return (
     <>
       <div className="w-[70%] h-full bg-red-100 m-auto p-[10%]">
-        <img src="" alt="" />
-        <div className="">
+        <img
+          src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+          alt=""
+        />
+        <div className="content">
           <h1>Title</h1>
+          <h2>Price</h2>
+          <p>description</p>
+          <Link>Edit</Link>
+          <Link>Delete</Link>
         </div>
       </div>
     </>
-  )
+  );
 }
 
 export default Details;

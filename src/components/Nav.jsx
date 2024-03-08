0b833d0bf3d4d0dab5914358fa364014bdc3746f
ApoorvaRaj123 <99551949+ApoorvaRaj123@users.noreply.github.com>
2024-03-08 21:44:31 +0530

    Add category name from API to Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,15 @@
-import { React } from "react";
+import { React, useContext } from "react";
+import { ProductContext } from "../utils/Context";
+import { Link } from "react-router-dom";
 
 function Nav() {
+  const { products } = useContext(ProductContext);
+
+  let distinct_category =
+    products && products.reduce((acc, cv) => [...acc, cv.category], []);
+  distinct_category = [...new Set(distinct_category)];
+  
+
   return (
     <>
       <nav className="w-[15%] h-full bg-zinc-100 flex flex-col items-center pt-5">
@@ -13,18 +22,16 @@ function Nav() {
         <hr className="my-3 w-[80%]" />
         <h1 className="text-xl font-semibold mb-3 w-[80%]">Categories</h1>
         <ul className="w-[80%]">
-          <li className="flex items-center gap-3 mb-3">
-            <span className="rounded-full block w-[17px] h-[17px] bg-blue-200"></span>{" "}
-            Cat 1
-          </li>
-          <li className="flex items-center gap-3 mb-3">
-            <span className="rounded-full block w-[17px] h-[17px] bg-red-200"></span>{" "}
-            Cat 2
-          </li>
-          <li className="flex items-center gap-3 mb-3">
-            <span className="rounded-full block w-[17px] h-[17px] bg-green-200"></span>{" "}
-            Cat 3
-          </li>
+          {distinct_category.map((c, i) => (
+            <Link
+              key={i}
+              to={`/?category=${c}`}
+              className="flex items-center gap-3 mb-3"
+            >
+              <span className="rounded-full block w-[17px] h-[17px] bg-blue-200"></span>{" "}
+              {c}
+            </Link>
+          ))}
         </ul>
       </nav>
     </>

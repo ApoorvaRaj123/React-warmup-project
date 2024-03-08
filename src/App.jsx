0acc1da86d7396804240f808f98765b0e096d4f6
ApoorvaRaj123 <99551949+ApoorvaRaj123@users.noreply.github.com>
2024-03-08 21:33:17 +0530

    Add Home link to  App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import { useState } from "react";
-import { Routes,Route } from "react-router-dom";
+import { Link, Routes, Route, useLocation } from "react-router-dom";
 import Home from "./components/Home";
 import Details from "./components/Details";
 
 function App() {
+  const { search, pathname } = useLocation();
+  console.log(search, pathname);
+
   return (
     <>
       <div className="h-screen w-full">
+        {(pathname != "/" || search.length > 0) && (
+          <Link to={"/"} className="absolute left-[17%] top-[2%] text-red-300">
+            Home
+          </Link>
+        )}
+
         <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/details/:id" element={<Details />}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/details/:id" element={<Details />} />
         </Routes>
       </div>
     </>
